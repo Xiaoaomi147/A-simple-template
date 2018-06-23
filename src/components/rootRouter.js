afa515e1f937a37ee4465fe6f9d5975a2e5f2988
Xiaoaomi147 <sf574495074@163.com>
2018-06-23 14:56:40 +0800

    就试一次

--- src/components/rootRouter.js
+++ src/components/rootRouter.js
@@ -2,12 +2,12 @@
  * @Author: shifan
  * @Date: 2018-06-12 15:27:41
  * @Last Modified by: shifan
- * @Last Modified time: 2018-06-21 17:18:07
+ * @Last Modified time: 2018-06-23 14:54:22
  * @功能: {}
  */
 
 import React from 'react';
-import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Redirect,BrowserRouter } from 'react-router-dom';
 import 'normalize.css';
 import Login from './Login/index';
 import Main from './Main/index';
