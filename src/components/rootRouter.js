7503be2ad7713d28b06190c678b3c0058941ca6e
Xiaoaomi147 <sf574495074@163.com>
2018-06-23 15:12:17 +0800

    werew

--- src/components/rootRouter.js
+++ src/components/rootRouter.js
@@ -2,12 +2,12 @@
  * @Author: shifan
  * @Date: 2018-06-12 15:27:41
  * @Last Modified by: shifan
- * @Last Modified time: 2018-06-23 14:54:22
+ * @Last Modified time: 2018-06-23 15:04:20
  * @功能: {}
  */
 
 import React from 'react';
-import { Switch, Route, Redirect,BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 import 'normalize.css';
 import Login from './Login/index';
 import Main from './Main/index';
